c92b24e63f160931692af9c878e62b5e5aba1a35
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2026-06-06 01:57:19 +0000

    Implement email link generation and browser automation
    Added a new script for generating a verification link using the Catchmail API and handling browser automation with SeleniumBase.

--- mind.py
+++ mind.py
@@ -1,1 +1,47 @@
+# raw__turnstile.py
+from seleniumbase import sb_cdp
+import random
+from seleniumbase import SB
+import requests
+import re
+import time
+import os
 
+os.makedirs("screenshots", exist_ok=True)
+
+
+def generate_vlink(c):
+    k=0
+
+    while k<10:
+        url = f"https://api.catchmail.io/api/v1/mailbox?address=ytprojectelevenlabsacckbkb{c}@catchmail.io"
+        r = requests.request("GET", url).json()
+        if r["count"]>0:
+            url = f"https://api.catchmail.io/api/v1/message/{r["messages"][0]["id"]}?mailbox=ytprojectelevenlabsacckbkb{c}@catchmail.io"
+            r = requests.request("GET", url).json()
+
+            return re.findall(r"(https?://[^\s)]+)(?=\s\))", (r["body"]["text"]))[1]        
+        else:
+            k+=1
+            time.sleep(4)
+    
+    raise Exception("ERRROR")
+
+url = "https://www.browserscan.net/bot-detection"
+
+w=random.randint(9999, 12000)
+
+print(w)
+
+try:
+    sb = sb_cdp.Chrome(url,
+    headed=True, 
+    chromium_arg="--no-sandbox")
+
+    sb.sleep(4)
+
+    ask=input("assk")
+
+    
+finally:
+    sb.driver.stop()
