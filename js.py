67505458e9b50d7ca9f541eb7fd4c63a20ec3439
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2026-06-06 09:01:52 +0000

    Add files via upload

--- js.py
+++ js.py
@@ -5,15 +5,11 @@ from seleniumbase import SB
 import requests
 import re
 import time
-import os
-
-os.makedirs("screenshots", exist_ok=True)
-
 
 def generate_vlink(c):
     k=0
 
-    while k<10:
+    while k<15:
         url = f"https://api.catchmail.io/api/v1/mailbox?address=ytprojectelevenlabsacckbkb{c}@catchmail.io"
         r = requests.request("GET", url).json()
         if r["count"]>0:
@@ -33,28 +29,17 @@ w=random.randint(9999, 12000)
 
 print(w)
 
-sb = sb_cdp.Chrome(
-    url,
-    test=True,
-    guest_mode=True
-)
+sb = sb_cdp.Chrome(url, test=True, guest_mode=True)
 
 try:
+
     sb.sleep(4)
 
-    sb.click('input[name="email"]')
-    sb.gui_press_keys(f"ytprojectelevenlabsacckbkb{9990+w}@catchmail.io")
-
-    sb.click('input[name="password"]')
-    sb.gui_press_keys(f"Prince!4438#")
-
-    sb.save_screenshot("newmethod.png", "screenshots")
-
-    # sb.press_keys('input[name="email"]', f"ytprojectelevenlabsacckbkb{9990+w}@catchmail.io")  # human-speed
-    # sb.press_keys('input[name="password"]', "Prince!4438#")
+    sb.press_keys('input[name="email"]', f"ytprojectelevenlabsacckbkb{9990+w}@catchmail.io", 20)  # human-speed
+    sb.press_keys('input[name="password"]', "Prince!4438#")
 
     # Standard syntax for clicking a button containing specific text in  Mode
-    sb.gui_click_element('div[data-testid="signup-signup-button-div"] button:contains("Sign up")')
+    sb.click('div[data-testid="signup-signup-button-div"] button:contains("Sign up")')
 
     sb.sleep(0.2)
 
@@ -63,10 +48,6 @@ try:
     sb.sleep(2)
 
     sb.save_screenshot("after sign up.png", "screenshots")
-
-    sb.sleep(4)
-
-    sb.save_screenshot("after sign up after 2.png", "screenshots")
 
     link=generate_vlink(9990+w)
 
@@ -85,7 +66,7 @@ try:
     sb.sleep(1)
 
     # Standard syntax for clicking a button containing specific text in  Mode
-    sb.gui_click_element('#sign-in-form > div.relative.flex.items-center.justify-between.w-full.h-fit.mt-4 > div.relative.w-full > button:contains("Sign in")')
+    sb.click('#sign-in-form > div.relative.flex.items-center.justify-between.w-full.h-fit.mt-4 > div.relative.w-full > button:contains("Sign in")')
 
     sb.sleep(2)
 
@@ -95,7 +76,7 @@ try:
 
     sb.sleep(3)
 
-    sb.gui_click_element('div[class="checkbox-hitarea overlay -inset-1.5"]')
+    sb.click('div[class="checkbox-hitarea overlay -inset-1.5"]')
 
     sb.click('button:contains("Next")')
 
@@ -131,7 +112,6 @@ try:
     element = sb.find_element('input[data-agent-id*="input-_r_"][readonly=""][type="text"]')
     attribute_value = element.get_attribute('value')
     print(f"value attribute: {attribute_value}")
-
 finally:
     sb.driver.stop()
 
